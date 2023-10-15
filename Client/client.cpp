ad14ccee840c6695ba000b74b3d9e3f9053288b6
SM Arif Ali <56111990+ArifAli-0@users.noreply.github.com>
2023-10-16 02:09:36 +0530

    client.cpp fixed

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,25 +1,24 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<netdb.h>
-#include<cstring>
-#include<arpa/inet.h>
-#include<fstream>
-#include<unistd.h>
-#include<chrono>
-#include<vector>
-#include<filesystem>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <netdb.h>
+#include <cstring>
+#include <arpa/inet.h>
+#include <fstream>
+#include <unistd.h>
+#include <chrono>
+#include <vector>
+#include <filesystem>
 
 using namespace std;
 
-
-
-class Client{
+class Client
+{
     int sockfd;
     string program_filename;
     string server_info;
-    int  iterations;
-    int  sleep_time;
+    int iterations;
+    int sleep_time;
     int success;
     vector<double> response_time;
     struct sockaddr_in server_addr;
@@ -27,80 +26,86 @@ class Client{
     void send_file()
     {
         uint32_t file_size = filesystem::file_size(program_filename);
-        cout<<"Hello World 1"<<file_size<<endl;
+        // cout << "File size" << file_size << endl;
         uint32_t length_to_send = htonl(file_size);
         // uint32_t length_to_send = file_size;
-        
 
-        if(write(sockfd,&length_to_send,sizeof(length_to_send)) <0)
-        throw("error sending file size");
+        if (write(sockfd, &length_to_send, sizeof(length_to_send)) < 0)
+            throw("error sending file size");
 
-        ifstream fin(program_filename,ios::binary);
-        if(!fin) throw("error opening file");
+        ifstream fin(program_filename, ios::binary);
+        if (!fin)
+            throw("error opening file");
         char buffer[1024];
-        cout<<"Hello World 2"<<endl;
-        while(!fin.eof())
+        // cout << "Sending file.." << endl;
+        while (!fin.eof())
         {
-            memset(buffer,0,sizeof(buffer));
-            fin.read(buffer,sizeof(buffer));
+            memset(buffer, 0, sizeof(buffer));
+            fin.read(buffer, sizeof(buffer));
             int k = fin.gcount();
-            //todo: handle if kernel fails to send the data
-            int n = write(sockfd,buffer,k);
-            if(n<0) throw ("error writing file");
+            // todo: handle if kernel fails to send the data
+            int n = write(sockfd, buffer, k);
+            if (n < 0)
+                throw("error writing file");
         }
-        cout<<"Hello World 3"<<endl;
+        // cout << "File sent" << endl;
     }
 
     void receive_response()
     {
         string response = "";
         uint32_t message_size;
-        if( read(sockfd,&message_size,sizeof(message_size)) <0) throw("file size read error");
-        // message_size=ntohl(message_size);
-        cout<<"Hello World 4"<<message_size<<endl;
+        if (read(sockfd, &message_size, sizeof(message_size)) < 0)
+            throw("file size read error");
+        message_size = ntohl(message_size);
+        // cout << "Received file size " << message_size << endl;
 
-        char buffer[1024];      
-        uint32_t read_bytes=0;
-cout<<"Hello World 5"<<endl;
-        while(read_bytes < message_size)
+        char buffer[1024];
+        uint32_t read_bytes = 0;
+        // cout << "Receiving file" << endl;
+        while (read_bytes < message_size)
         {
-            memset(buffer,0,sizeof(buffer));
-            uint32_t current_read = read(sockfd,buffer,sizeof(buffer));
-          //  if(current_read ==0 ) throw("socket read error");
+            memset(buffer, 0, sizeof(buffer));
+            uint32_t current_read = read(sockfd, buffer, sizeof(buffer));
+            //  if(current_read ==0 ) throw("socket read error");
             response += string(buffer);
-            read_bytes += current_read;     
+            read_bytes += current_read;
         }
-cout<<"Hello World 6"<<endl;
-        cout<<response<<endl;
+        // cout << "Received: " << endl;
+        cout << response << endl;
     }
 
-    void setup_socket(){
+    void setup_socket()
+    {
         int t1 = server_info.find(":");
-        string ip = server_info.substr(0,t1);
-        string t2 = server_info.substr(t1+1);
+        string ip = server_info.substr(0, t1);
+        string t2 = server_info.substr(t1 + 1);
         int port = stoi(t2);
 
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0)  throw("Error opening socket");
-        
-        memset(&server_addr,0,sizeof(server_addr));
-        server_addr.sin_family=AF_INET;
-        server_addr.sin_port=htons(port);
-        server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
-        int sc = connect(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr));
-        if(sc < 0) throw("Cannot connect");
+        if (sockfd < 0)
+            throw("Error opening socket");
 
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(port);
+        server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+        int sc = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (sc < 0)
+            throw("Cannot connect");
     }
-    public:
+
+public:
     void submit()
     {
-        for(int i =0; i<iterations;i++)
-        {   setup_socket();
+        for (int i = 0; i < iterations; i++)
+        {
+            setup_socket();
             auto start_time = chrono::high_resolution_clock::now();
             send_file();
             receive_response();
             auto end_time = chrono::high_resolution_clock::now();
-            double total_time = chrono::duration_cast<chrono::milliseconds>(end_time-start_time).count();
+            double total_time = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
             response_time.push_back(total_time);
             success++;
             close(sockfd);
@@ -108,20 +113,18 @@ cout<<"Hello World 6"<<endl;
         }
     }
 
-    Client(string server_info,string program_filename,int iterations, int sleep_time):server_info(server_info),\
-    program_filename(program_filename),iterations(iterations),sleep_time(sleep_time){
-        success=0;
-
-        setup_socket();
+    Client(string server_info, string program_filename, int iterations, int sleep_time) : server_info(server_info),
+                                                                                          program_filename(program_filename), iterations(iterations), sleep_time(sleep_time)
+    {
+        success = 0;
     };
-    
 };
 
-
 int main(int argc, char const *argv[])
-{  
+{
     // Process arguments
-    if (argc != 5 ) {
+    if (argc != 5)
+    {
         throw("Usage : ./client <serverip:port> <filetosubmit> <loopNum> <sleepTime> ");
         exit(0);
     }
@@ -130,11 +133,12 @@ int main(int argc, char const *argv[])
     int iterations = stoi(argv[3]);
     int sleeptime = stoi(argv[4]);
 
-    try{
-    Client client(server_info,program_filename,iterations,sleeptime);
-    client.submit();
+    try
+    {
+        Client client(server_info, program_filename, iterations, sleeptime);
+        client.submit();
     }
-    catch(const char * msg)
+    catch (const char *msg)
     {
         cerr << msg << '\n';
     }
